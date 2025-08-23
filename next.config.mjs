59e854c55fae2e81541bfec9916eb5ad6c778e04
Justice <ezejustice112@gmail.com>
2025-08-23 02:33:28 +0100

    added latest change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true
+  },
+  basePath: "/SwiftCare-Platform-WebApp",
+  assetPrefix: "/SwiftCare-Platform-WebApp"
+};
 
 export default nextConfig;
+
